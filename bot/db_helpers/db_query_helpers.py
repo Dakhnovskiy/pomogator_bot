dfeab06e6939f37a56d39e12fb44917f5e2efee0
dezman <dmitriy_dakhnovskiy@mail.ru>
2018-02-05 19:56:30 +0300

    # баг в and условии запроса

--- bot/db_helpers/db_query_helpers.py
+++ bot/db_helpers/db_query_helpers.py
@@ -6,9 +6,8 @@ def get_params_from_last_request(chat_id, command):
     session = get_session()
 
     last_request = session.query(Request).filter(
-        Request.chat_id == chat_id and Request.command == command
+        Request.chat_id == chat_id, Request.command == command
     ).order_by(Request.date.desc()).first()
-
     params = {}
     if last_request:
         for param in last_request.params:
